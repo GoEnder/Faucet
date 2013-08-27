d72780d5d9380dd089d7e1ba6584bf4f8c220af8
Joshua Baldwin <iam@joshuabaldwin.eu>
2013-08-28 00:50:41 +0100

    Apply Faucet Patches only (For now!)

--- applyPatches.sh
+++ applyPatches.sh
@@ -1,1 +1,47 @@
 #!/bin/bash
+
+ORIG_PWD="$(pwd)"
+cd "$(dirname "$0")"
+echo "  Rebuilding Forked projects.... "
+
+function applyFaucetPatches {
+    what=base/$1
+    target=build/$2
+    patches=patches/Faucet/$1
+
+    cd $what
+    git branch -f upstream >/dev/null
+    cd ../../
+    if [ ! -d  $target ]; then
+        git clone $what $target -b upstream
+    fi
+
+    cd $target
+    echo "  Resetting $target to $what..."
+    git remote rm upstream 2>/dev/null 2>&1
+    git remote add upstream ../../$what >/dev/null 2>&1
+    git checkout master >/dev/null 2>&1
+    git fetch upstream >/dev/null 2>&1
+    git reset --hard upstream/upstream
+
+    echo "  Applying patches to $target..."
+    git am --abort
+
+    if !(git am --3way ../../$patches/*.patch); then
+        echo "  Something did not apply cleanly to $target."
+        echo "  Please review above details and finish the apply then"
+        echo "  save the changes with rebuildPatches.sh"
+        cd "$ORIG_PWD"
+        exit $?
+    else
+        echo "  Patches applied cleanly to $target"
+    fi
+
+    cd ../..
+}
+
+applyFaucetPatches Bukkit Faucet-API
+applyFaucetPatches CraftBukkit Faucet
+
+
+cd "$ORIG_PWD"

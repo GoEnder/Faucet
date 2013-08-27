3c2c3c092616148c751c70dcd8193f3789999b63
Joshua Baldwin <iam@joshuabaldwin.eu>
2013-08-28 00:45:39 +0100

    Simple patching system for just Faucet files.
    Will be adding Spigot and Sportbukkit soon.

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+basedir=`pwd`
+
+cd "$(dirname "$0")"
+echo "Rebuilding patch files from current fork state..."
+
+function cleanupPatches {
+    cd $1
+    for patch in *.patch; do
+        gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
+        diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
+
+        testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
+        if [ "x$testver" != "x" ]; then
+            diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
+        fi
+
+        if [ "x$diffs" == "x" ] ; then
+            git reset HEAD $patch >/dev/null
+            git checkout -- $patch >/dev/null
+        fi
+    done
+    cd ..
+}
+
+function rebuildPatches {
+    what=base/$1
+    target=build/$2
+    patches=patches/Faucet/$1
+    cd $target
+    git format-patch --no-stat -N -o ../../$patches upstream/upstream
+    cd ../..
+    git add $patches
+    cleanupPatches $patches
+    echo "  Patches saved for $what to $patches"
+}
+
+rebuildPatches Bukkit Faucet-API
+rebuildPatches CraftBukkit Faucet
